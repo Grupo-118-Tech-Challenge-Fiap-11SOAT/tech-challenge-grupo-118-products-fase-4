6740e27735ada5b8d8c229bac6783c6b0246b774
tuliorezende <tulio.rezende@outlook.com>
2025-12-30 20:49:35 -0300

    feat: enhance GetProductByTypeUseCaseTests with specific product type tests for drinks, snacks, desserts, and accompaniments

--- Products/Products.UnitTests/Products.Application/UseCases/GetProductByTypeUseCaseTests.cs
+++ Products/Products.UnitTests/Products.Application/UseCases/GetProductByTypeUseCaseTests.cs
@@ -19,8 +19,9 @@ public class GetProductByTypeUseCaseTests
         _productRepositoryMock = new Mock<IProductRepository>();
         _useCase = new GetProductByTypeUseCase(_productRepositoryMock.Object);
     }
+    
     [Fact]
-    public async Task ExecuteAsync_WhenProductsExist_ShouldReturnOk()
+    public async Task ExecuteAsync_WhenDrinkProductsExist_ShouldReturnOk()
     {
         //Arrange
         var cocaId = ObjectId.GenerateNewId();
@@ -28,7 +29,7 @@ public class GetProductByTypeUseCaseTests
 
         List<ImageProductDto> imagesProductDto = new List<ImageProductDto>();
 
-        List<DrinkDto> expectedData = new List<DrinkDto>{
+        List<ProductDto> expectedData = new List<ProductDto>{
             new DrinkDto(){Id = cocaId.ToString(), Name = "Coca-Cola", Price = 12.50m, IsActive = true, Images = imagesProductDto},
             new DrinkDto(){Id = juiceId.ToString(), Name = "Suco de Uva", Price = 12.50m, IsActive = true, Images = imagesProductDto, Size = "P", Flavor = "Uva" }
             };
@@ -50,7 +51,91 @@ public class GetProductByTypeUseCaseTests
         result.Data.Should().BeEquivalentTo(expectedData);
         result.StatusCode.Should().Be(HttpStatusCode.OK);
     }
+    
+    [Fact]
+    public async Task ExecuteAsync_WhenSnackProductsExist_ShouldReturnOk()
+    {
+        //Arrange
+        var hamburgerId = ObjectId.GenerateNewId();
+        List<ImageProductDto> imagesProductDto = new List<ImageProductDto>();
 
+        List<ProductDto> expectedData = new List<ProductDto>{
+            new SnackDto(){Id = hamburgerId.ToString(), Name = "Hambúrguer", Price = 20.00m, IsActive = true, Images = imagesProductDto, Ingredients = ["Pão, carne, queijo, alface, tomate"] }
+        };
+
+        List<ImageProduct> imagesProduct = new List<ImageProduct>();
+
+        var productsEntities = new List<Product>()
+        {
+            new Snack(hamburgerId, "Hambúrguer", 20.00m, true, DateTime.Now, DateTime.Now,["Pão, carne, queijo, alface, tomate"], imagesProduct)
+        };
+
+        _productRepositoryMock.Setup(x => x.GetProductByTypeAsync(It.IsAny<string>(), It.IsAny<CancellationToken>())).ReturnsAsync(productsEntities);
+
+        //Act
+        var result = await _useCase.ExecuteAsync("123", CancellationToken.None);
+
+        //Assert
+        result.Data.Should().BeEquivalentTo(expectedData);
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+
+    [Fact]
+    public async Task ExecuteAsync_WhenDessertProductsExist_ShouldReturnOk()
+    {
+        //Arrange
+        var cheesecakeId = ObjectId.GenerateNewId();
+        List<ImageProductDto> imagesProductDto = new List<ImageProductDto>();
+
+        List<ProductDto> expectedData = new List<ProductDto>{
+            new DessertDto(){Id = cheesecakeId.ToString(), Name = "Pudim", Price = 15.00m, IsActive = true, Images = imagesProductDto, PortionSize = "Médio" },
+        };
+
+        List<ImageProduct> imagesProduct = new List<ImageProduct>();
+
+        var productsEntities = new List<Product>()
+        {
+            new Dessert(cheesecakeId, "Pudim", 15.00m, true, "Médio", DateTime.Now, DateTime.Now, imagesProduct),
+        };
+
+        _productRepositoryMock.Setup(x => x.GetProductByTypeAsync(It.IsAny<string>(), It.IsAny<CancellationToken>())).ReturnsAsync(productsEntities);
+
+        //Act
+        var result = await _useCase.ExecuteAsync("123", CancellationToken.None);
+
+        //Assert
+        result.Data.Should().BeEquivalentTo(expectedData);
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+    
+    [Fact]
+    public async Task ExecuteAsync_WhenAccompanimentProductsExist_ShouldReturnOk()
+    {
+        //Arrange
+        var frenchFriesId = ObjectId.GenerateNewId();
+        List<ImageProductDto> imagesProductDto = new List<ImageProductDto>();
+
+        List<ProductDto> expectedData = new List<ProductDto>{
+            new AccompanimentDto(){Id = frenchFriesId.ToString(), Name = "Batata Frita", Price = 10.00m, IsActive = true, Images = imagesProductDto, Size = "Grande" },
+        };
+
+        List<ImageProduct> imagesProduct = new List<ImageProduct>();
+
+        var productsEntities = new List<Product>()
+        {
+            new Accompaniment(frenchFriesId, "Batata Frita", 10.00m, true, DateTime.Now, DateTime.Now, "Grande", imagesProduct),
+        };
+
+        _productRepositoryMock.Setup(x => x.GetProductByTypeAsync(It.IsAny<string>(), It.IsAny<CancellationToken>())).ReturnsAsync(productsEntities);
+
+        //Act
+        var result = await _useCase.ExecuteAsync("123", CancellationToken.None);
+
+        //Assert
+        result.Data.Should().BeEquivalentTo(expectedData);
+        result.StatusCode.Should().Be(HttpStatusCode.OK);
+    }
+    
     [Fact]
     public async Task ExecuteAsync_WhenProductNotExist_ShouldReturnFail()
     {
