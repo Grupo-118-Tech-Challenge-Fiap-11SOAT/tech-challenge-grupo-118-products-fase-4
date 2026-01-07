39d7452aef8f3fc6c46bfe3e08ddc24998ca6404
DESKTOP-2IJI07V\sabri <sabrina.o.cardoso@outlook.com>
2026-01-07 20:56:55 -0300

    tests

--- Products/Products.Domain/Common/Exceptions/InvalidObjectIdException.cs
+++ Products/Products.Domain/Common/Exceptions/InvalidObjectIdException.cs
@@ -1,5 +1,8 @@
-﻿namespace Products.Domain.Common.Exceptions;
+﻿using System.Diagnostics.CodeAnalysis;
 
+namespace Products.Domain.Common.Exceptions;
+
+[ExcludeFromCodeCoverage]
 public class InvalidObjectIdException : Exception
 {
     public InvalidObjectIdException(string value)
